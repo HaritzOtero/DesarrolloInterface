c708c11833d1cec3210f31838ca8fada1c3166d7
Haritz Otero <otero.haritz@uni.eus>
2022-12-19 12:33:55 +0100

    19/12/2022

---
+++ C#/ASP.NET-MVC/WineShop/WineShop/ViewComponents/KarritoaViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WineShop.Models;
+using WineShop.Services;
+
+namespace WineShop.ViewComponents
+{
+    public class KarritoaViewComponent : ViewComponent
+    {
+        private readonly ISaskiaService _saskiaService;
+        public KarritoaViewComponent(ISaskiaService saskiaService)
+        {
+            _saskiaService = saskiaService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+
+            List<SaskiaAlea> count = await _saskiaService.SaskiaLortuAleak(id);
+
+            ViewBag.saskiacount = count.Count();
+            return View();
+        }
+    }
+}
